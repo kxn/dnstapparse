d678257a885b81a038edc78c6bc8fe005f9dbb1c
kangxn <kangxn@gmail.com>
2021-03-02 21:39:47 +0800

    added proxy ip check

--- main.go
+++ main.go
@@ -17,10 +17,24 @@ import (
 var (
 	counter    sync.Map
 	zoneList   = []string{}
+	hostlist   = map[string]bool{}
 	quiet      = flag.Bool("quiet", false, "do not print dnstap messages on stdout")
 	listenAddr = flag.String("listen", "127.0.0.1:5678", "address to listen to")
 	zoneFile   = flag.String("zonefile", "/etc/unbound/local.d/dns.conf", "unbound local-data file")
+	proxyIPs   = flag.String("proxyips", "", "ip list of proxies")
+	proxyList  map[string]bool
 )
+
+func isProxyIP(ip string) bool {
+	if proxyList == nil {
+		proxyList = map[string]bool{}
+		for _, i := range strings.Split(*proxyIPs, ",") {
+			proxyList[i] = true
+		}
+	}
+	_, ok := proxyList[ip]
+	return ok
+}
 
 func normalize(s string) string {
 	s = strings.ToLower(s)
@@ -32,6 +46,8 @@ func normalize(s string) string {
 
 func loadZoneFile(file string) error {
 	zoneRe := regexp.MustCompile(`local-zone:\s*\"*([^\s^\"]+)\"*\s+([\w]+)`)
+	dataRe := regexp.MustCompile(`local-data:\s*\"([^\"]+)\"`)
+	aRe := regexp.MustCompile(`([^\s]+)\s+([\d]+)\s+in\s+a\s+([\d\.]+)`)
 	f, err := os.Open(file)
 	if err != nil {
 		return err
@@ -46,6 +62,18 @@ func loadZoneFile(file string) error {
 			if m != nil && len(m) == 1 && len(m[0]) == 3 && m[0][2] == "redirect" {
 				zonemap[normalize(m[0][1])] = true
 			}
+			continue
+		}
+		if strings.Contains(line, "local-data:") {
+			m := dataRe.FindAllStringSubmatch(line, 1)
+			if m != nil && len(m) == 1 && len(m[0]) == 2 {
+				m1 := aRe.FindAllStringSubmatch(line, 1)
+				if m1 != nil {
+					if isProxyIP(m[0][3]) {
+						hostlist[normalize(m1[0][1])] = true
+					}
+				}
+			}
 		}
 	}
 
@@ -53,7 +81,10 @@ func loadZoneFile(file string) error {
 		return err
 	}
 	for k := range zonemap {
-		zoneList = append(zoneList, k)
+		_, ok := hostlist[k]
+		if ok {
+			zoneList = append(zoneList, k)
+		}
 	}
 	return nil
 }
